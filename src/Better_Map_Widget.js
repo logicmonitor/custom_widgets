3506620d4c1c0a06bce7a96fde0d6d1e7ceec22f
Kevin Ford <kevin.ford@logicmonitor.com>
2025-11-20 15:17:53 -0600

    Update Better_Map_Widget.js

--- src/Better_Map_Widget.js
+++ src/Better_Map_Widget.js
@@ -1,6 +1,6 @@
 // Better Map Widget
 // Developed by Kevin Ford
-// Version 2.80 - Integrated Authentication Version
+// Version 2.81 - Integrated Authentication Version
 // Integrated authentication improvements by Steve Villardi
 
 // Some of the ideas behind this project:
